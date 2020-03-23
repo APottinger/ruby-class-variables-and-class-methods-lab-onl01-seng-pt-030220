a567cfa01936275af78679271a9cb70821a0baa2
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-23 19:45:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,17 +17,9 @@ class Song
     @@artists << artist 
   end
   
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
+ def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
   
   def self.artist_count
     artist_count = {}
